557ad07ac3179f9fdb7a8f59755983a24f261239
fdaniel13s <u20221c628@upc.edu.pe>
2024-06-20 22:25:58 -0500

    chore: add settings to deploy v1.

--- src/main/java/com/prodev/ecomarket/WebServicesEcomarketApplication.java
+++ src/main/java/com/prodev/ecomarket/WebServicesEcomarketApplication.java
@@ -19,7 +19,7 @@ public class WebServicesEcomarketApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("*")
+						.allowedOriginPatterns("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
