24009c56aa6db409bf5bc9bf0230df946acfa2a7
fdaniel13s <u20221c628@upc.edu.pe>
2024-06-26 16:08:02 -0500

    added CORS configuration.

--- src/main/java/com/prodev/ecomarket/WebServicesEcomarketApplication.java
+++ src/main/java/com/prodev/ecomarket/WebServicesEcomarketApplication.java
@@ -2,6 +2,9 @@ package com.prodev.ecomarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,6 +15,16 @@ public class WebServicesEcomarketApplication {
 		SpringApplication.run(WebServicesEcomarketApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
-
-
