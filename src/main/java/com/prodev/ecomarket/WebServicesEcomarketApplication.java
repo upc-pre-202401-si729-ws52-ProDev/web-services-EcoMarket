929ee93fcff2e9a3bd8eb193ae1c5f2317ea069f
Fernando Daniel Quispe Condori <130398241+fdaniel13s@users.noreply.github.com>
2024-06-27 12:03:48 -0500

    Update WebServicesEcomarketApplication.java

--- src/main/java/com/prodev/ecomarket/WebServicesEcomarketApplication.java
+++ src/main/java/com/prodev/ecomarket/WebServicesEcomarketApplication.java
@@ -15,16 +15,4 @@ public class WebServicesEcomarketApplication {
 		SpringApplication.run(WebServicesEcomarketApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*");
-			}
-		};
-	}
 }
